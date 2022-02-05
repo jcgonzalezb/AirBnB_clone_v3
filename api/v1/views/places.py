9cbfe7ced2cc81aa4a8123f0b53ca22a51727709
jcgonzalezb <jcgonzalez@gmail.com>
2022-02-05 12:36:51 -0500

    get method fixed

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -20,23 +20,20 @@ def list_places():
 
 @app_views.route('/cities/<city_id>/places', methods=['GET'], strict_slashes=False)
 def all_places(city_id):
-    
     """ returns list of all Place objects linked to a given City """
     city = storage.get("City", city_id)
     if city is None:
         abort(400)
     places_all = []
-    places = storage.get("Place")
     for place in city.places:
         if place.city_id == city_id:
             places_all.append(place.to_dict())
-    
     return jsonify(places_all)
 
 @app_views.route('/places/<places_id>',  methods=['GET'], strict_slashes=False)
-def places_get(place_id):
+def places_get(places_id):
     """ Handles GET method """
-    place = storage.get("Place", place_id)
+    place = storage.get("Place", places_id)
     if place is None:
         abort(404)
     place = place.to_dict()
@@ -44,20 +41,20 @@ def places_get(place_id):
 
 @app_views.route('/cities/<city_id>/places', methods=['POST'],
                  strict_slashes=False)
-def places_post(city_id):
-    """Handles POst method"""
+def create_places(city_id):
+    """Creates a place. Handles POST method"""
     city = storage.get("City", city_id)
     if city is None:
         abort(404)
     data = request.get_json()
     if data is None:
         abort(400, "Not a JSON")
-    if 'user_id' not in data.keys():
+    if 'user_id' not in data:
         abort(400, "Missing user_id")
     user = storage.get("User", data['user_id'])
     if user is None:
         abort(404)
-    if 'name' not in data.keys():
+    if 'name' not in data:
         abort(400, "Missing name")
     place = Place(**data)
     place.city_id = city_id
