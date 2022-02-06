3aa94ed3619d89410690dbfc94898937a3168b15
jcgonzalezb <jcgonzalez@gmail.com>
2022-02-05 20:44:00 -0500

    ignore keys added

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -54,24 +54,28 @@ def create_user():
         abort(400, "Missing password")
     else:
         new_user = User(**data)
-        storage.new(new_user)
         new_user.save()
         return jsonify(new_user.to_dict()), 201
 
 
-@app_views.route('/users/<user_id>', methods=['PUT'])
+@app_views.route('/users/<user_id>', methods=['PUT'],
+                 strict_slashes=False)
 def user_put(user_id):
     """ Updates a User. Handles PUT method """
-    user = storage.get("User", user_id)
-    if user is None:
-        abort(404)
-    data = request.get_json()
+    data = request.get_json(silent=True)
     if data is None:
         abort(400, "Not a JSON")
-    for key, value in data.items():
-        ignore_keys = ["id", "email", "created_at", "updated_at"]
-        if key not in ignore_keys:
-            user.bm_update(key, value)
+
+    obj = storage.get("User", user_id)
+    if obj is None:
+        abort(404)
+
+    ignore_keys = ["id", "email", "created_at", "updated_at"]
+    for key in data.keys():
+        if key in ignore_keys:
+            pass
+        else:
+            setattr(obj, key, data[key])
     storage.save()
-    res = user.to_dict()
+    res = obj.to_dict()
     return jsonify(res), 200
