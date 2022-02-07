9697aa29a6728abad87933c2ccf7e9e11a5d9423
jcgonzalezb <jcgonzalez@gmail.com>
2022-02-07 14:34:31 -0500

    import added

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -7,6 +7,7 @@ import pep8 as pycodestyle
 import time
 import unittest
 from unittest import mock
+import sys
 BaseModel = models.base_model.BaseModel
 module_doc = models.base_model.__doc__
 
@@ -58,7 +59,6 @@ class TestBaseModelDocs(unittest.TestCase):
 
 class TestBaseModel(unittest.TestCase):
     """Test the BaseModel class"""
-
     def test_instantiation(self):
         """Test that object is correctly created"""
         inst = BaseModel()
